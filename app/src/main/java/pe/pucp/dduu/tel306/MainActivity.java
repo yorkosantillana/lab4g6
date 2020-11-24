6a22341c35d323c1801e7f8eab7c9fe0e7fff472
Yorkman Santillana <yorkman.santillana@gmail.com>
2020-11-24 08:16:32 -0500

    Creación de Proyecto

---
+++ app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
@@ -0,0 +1,14 @@
+package pe.pucp.dduu.tel306;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
