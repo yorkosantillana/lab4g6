52c23ca5e265f056913bd216a35731b796b2cd93
Leonardo <a20125980@pucp.pe>
2020-11-24 09:09:40 -0500

    mostrar Fragmento

--- app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
+++ app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
@@ -1,8 +1,11 @@
 package pe.pucp.dduu.tel306;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MostrarFragmento();
+    }
+
+    public void MostrarFragmento (){
+
+        RegistroUsuario_Fragment registroUsuario_fragment = RegistroUsuario_Fragment.newInstance();
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentInicioRegistroContainer,registroUsuario_fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
