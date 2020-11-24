bc1934fd066031aae93aa78fed816921fc412b45
Yorkman Santillana <yorkman.santillana@gmail.com>
2020-11-24 11:39:17 -0500

    implementación POST y GET usuario hacia API

--- app/src/main/java/pe/pucp/dduu/tel306/TestStorage.java
+++ app/src/main/java/pe/pucp/dduu/tel306/TestStorage.java
@@ -1,10 +1,15 @@
 package pe.pucp.dduu.tel306;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -14,6 +19,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -21,72 +27,145 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Base64;
 
 public class TestStorage extends AppCompatActivity {
 
+
+    private Button getApiBtn, postApiBtn;
+    private TextView resultTextView;
+    RequestQueue requestQueue;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_storage);
 
 
+        //###########################
+        //PARA CONSULTAR EL API
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+        getApiBtn = (Button) findViewById(R.id.getApiBtn);
+        postApiBtn = (Button)findViewById(R.id.postApiBtn);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        getApiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        });
+
+        postApiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postDataNEW();
+                System.out.println(requestQueue);
+            }
+        });
+        postApiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postDataLOGIN();
+                System.out.println(requestQueue);
+            }
+        });
+        //###########################
 
 
 
 
+
+
+
+
+
+    }
+
+
+
+
+    public void postDataLOGIN() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JSONObject object = new JSONObject();
         try {
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            String URL = "34.236.191.118:3000/api/v1/users/new";
-            JSONObject jsonBody = new JSONObject();
-            jsonBody.put("Title", "Android Volley Demo");
-            jsonBody.put("Author", "BNK");
-            final String requestBody = jsonBody.toString();
+            //input your API parameters
+            object.put("email", "yorko123@pucp.pe");
+            object.put("password", "12345");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Enter the correct url for your api service site
+        String url = "http://34.236.191.118:3000/api/v1/users/login";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, object,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        resultTextView.setText("String Response : "+ response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                resultTextView.setText("Error getting response");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
 
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+    public void postDataNEW() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JSONObject object = new JSONObject();
+        try {
+            //input your API parameters
+            object.put("email", "yorko123@pucp.pe");
+            object.put("password", "12345");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // Enter the correct url for your api service site
+        String url = "http://34.236.191.118:3000/api/v1/users/login";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, object,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        resultTextView.setText("String Response : "+ response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                resultTextView.setText("Error getting response");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+    // Get Request For JSONObject
+    public void getData(){
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        try {
+            String url = "http://34.236.191.118:3000/api/v1/users/login";
+            JSONObject object = new JSONObject();
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                 @Override
-                public void onResponse(String response) {
-                    Log.i("VOLLEY", response);
+                public void onResponse(JSONObject response) {
+                    resultTextView.setText("Resposne : " + response.toString());
+                    Toast.makeText(getApplicationContext(), "I am OK !" + response.toString(), Toast.LENGTH_LONG).show();
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.e("VOLLEY", error.toString());
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
                 }
-            }) {
-                @Override
-                public String getBodyContentType() {
-                    return "application/json; charset=utf-8";
-                }
-
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    try {
-                        return requestBody == null ? null : requestBody.getBytes("utf-8");
-                    } catch (UnsupportedEncodingException uee) {
-                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                        return null;
-                    }
-                }
-
-                @Override
-                protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                    String responseString = "";
-                    if (response != null) {
-                        responseString = String.valueOf(response.statusCode);
-                        // can get more details such as response.headers
-                    }
-                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-                }
-            };
-
-            requestQueue.add(stringRequest);
-
-        }catch(JSONException e){
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
 
 
 
